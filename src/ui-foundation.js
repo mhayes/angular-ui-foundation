0cfb9ba55deab2d7dd63eddf022b3c5f202ce827
Mark Hayes <mark@zurb.com>
2013-06-21 10:20:06 -0700

    add ability to calculate container width/height helper

--- src/ui-foundation.js
+++ src/ui-foundation.js
@@ -1,4 +1,35 @@
-angular.module("ui.foundation", ["ui.foundation.alert", "ui.foundation.reveal"]);
+angular.module("ui.foundation", ["ui.foundation.utils","ui.foundation.alert", "ui.foundation.reveal"]);
+
+angular.module("ui.foundation.utils", [])
+  .directive("containerSize", function() {
+    return {
+      restrict: "A",
+      controller: function($element, $window) {
+        angular.element($window).bind("resize", function(){
+          var el = angular.element($element);
+          el.css({width: "", height: ""});
+          el.css({
+            width: el.width(),height: el.height()
+          });
+        });
+      },
+      link: function(scope, el, attrs) {
+        console.info(attrs);
+        el.ready(function(){
+          el.css({width: "",height: ""});
+          el.css({
+            width: el.width(),height: el.height()
+          });
+        });
+        el.find("img").load(function(){
+          el.css({width: "",height: ""});
+          el.css({
+            width: el.width(),height: el.height()
+          });
+        });
+      }
+    }
+  });
 
 angular.module("ui.foundation.alert", [])
   .directive("alert", function($window) {
