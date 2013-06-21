430ed3e052e46e238210ad33fb5394f83a1869d0
Mark Hayes <mark@zurb.com>
2013-06-21 08:32:49 -0700

    add sections example

--- misc/assets/app.js
+++ misc/assets/app.js
@@ -21,3 +21,18 @@ app.controller("RevealDemoCntrl", ["$scope", function($scope){
     $scope.visible = true
   }
 }]);
+
+app.controller("SectionDemoCntrl", ["$scope", function($scope){
+  $scope.activeIdx = 0;
+  $scope.sections = [
+    {title: "Section 1", content: "Content 1 goes here!"},
+    {title: "Section 2", content: "Content 2 goes here!"},
+    {title: "Section 3", content: "Content 3 goes here!"}
+  ];
+  $scope.getClass = function(idx) {
+    if (idx === $scope.activeIdx) return "active";
+  };
+  $scope.setActiveIdx = function(idx) {
+    $scope.activeIdx = idx;
+  };
+}]);
