017121bf3ee9d39983ce5a54cb0681e3f4b035bf
Lefteris Paraskevas <lefteris.paraskevas@capgemini.com>
2013-09-26 18:24:05 +0100

    wip(slide-in): add simple example for slide in animation

--- src/app/reveal/reveal.js
+++ src/app/reveal/reveal.js
@@ -20,26 +20,26 @@ angular.module("foundation.reveal", [])
         };
       }
     };
-  })
-  .animation(".reveal-animation", function(){
-    return {
-      leave: function(element, done){
-        done();
-      },
-      addClass: function(element, className, done){
-        if (className === "ng-hide") {
-          var modal = element.find(".reveal-modal");
-          modal.animate({top:"-200px"}, 300, function(){ done(); });
-        }
-      },
-      removeClass: function(element, className, done) {
-        if (className === "ng-hide") {
-          element.show();
-          var modal = element.find(".reveal-modal");
-          modal.css("top","-200px");
-          done();
-          modal.animate({top:"50px"},300, function(){ done(); });
-        }
-      }
-    };
   });
+//  .animation(".reveal-animation", function(){
+//    return {
+//      leave: function(element, done){
+//        done();
+//      },
+//      addClass: function(element, className, done){
+//        if (className === "ng-hide") {
+//          var modal = element.find(".reveal-modal");
+//          modal.animate({top:"-200px"}, 300, function(){ done(); });
+//        }
+//      },
+//      removeClass: function(element, className, done) {
+//        if (className === "ng-hide") {
+//          element.show();
+//          var modal = element.find(".reveal-modal");
+//          modal.css("top","-200px");
+//          done();
+//          modal.animate({top:"50px"},300, function(){ done(); });
+//        }
+//      }
+//    };
+//  });
