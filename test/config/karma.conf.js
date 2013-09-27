95579b4880a6ae8003ef282cad90ea91e636dff9
Lefteris Paraskevas <lefteris.paraskevas@capgemini.com>
2013-09-27 19:07:23 +0100

    chore(test): change karma config so it can test directives with templateURL

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -1,15 +1,19 @@
 // Karma configuration
 module.exports = function(config) {
 	config.set({
+
 		// base path, that will be used to resolve files and exclude
 		basePath: '../../',
 
 		frameworks: ['jasmine'],
 		// list of files / patterns to load in the browser
 		files: [
+			'src/vendor/jquery/jquery.min.js',
 			'src/vendor/angular/angular.js',
+			'src/vendor/angular-mocks/angular-mocks.js',
 			'src/app/**/*.js',
-			'test/unit/**/*.spec.js'
+			'test/unit/**/*.spec.js',
+			'src/app/**/*.html'
 		],
 
 		// list of files to exclude
